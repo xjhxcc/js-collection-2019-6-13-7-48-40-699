c3bf867a6e94f4b57f197b80bb485a9fd4b46611
xjhxcc <2351889342@qq.com>
2019-07-10 15:13:32 +0800

    refactor:map

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,9 @@
 'use strict';
 var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+  return collection.map(item=>item*4+1);
+  //return [5,9,13,17,21];
 };
 
 module.exports = map_to_four_multiples_add_one;
+
+//node_modules/jasmine-node/bin/jasmine-node --verbose specs/map
